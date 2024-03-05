a2d657852a3e8845b31c43651422a9b5e14e900b
jose.zambrano <jomiguel60@gmail.com>
2024-03-05 22:37:33 +0100

    refactor de archivos de configuracion

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,53 +2,25 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   extends: [
     'plugin:react/recommended',
-    'airbnb',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
+    'prettier'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
-    // Permite console.log, pero muestra una advertencia en la consola
     'no-console': 'warn',
-    // Permite el uso de dependencias de desarrollo en los archivos de prueba y configuración
-    'import/no-extraneous-dependencies': [
-      'error',
-      { devDependencies: ['**/test.tsx', '**/test.ts', '**/*.config.js'] },
-    ],
-    // Permite la reasignación de propiedades de objetos (útil para trabajar con reducers)
-    'no-param-reassign': ['error', { props: false }],
-    // No requiere que las extensiones .tsx y .ts se especifiquen al importar
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    // No requiere declaraciones de tipo de propiedades con TypeScript
-    'react/prop-types': 'off',
-    // No requiere el uso de 'this' en los métodos de clase que no lo utilizan
-    'class-methods-use-this': 'off',
-    // No aplicar una máxima longitud de línea, dejando que Prettier se encargue del formateo
-    'max-len': 'off',
-    // Permitir ciertos métodos no modificadores en los arrays como .push
-    'no-array-constructor': 'off',
-    // No forzar el uso de desestructuración en objetos y arrays
-    'prefer-destructuring': 'off',
-  },
+    'no-unused-vars': 'warn',
+    'react/react-in-jsx-scope': 'off'
+  }
 };
