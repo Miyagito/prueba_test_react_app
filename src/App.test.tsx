ded56204be64944c4513a8ee5f0e381b7d05d35b
jose.zambrano <jomiguel60@gmail.com>
2024-03-06 11:44:01 +0100

    eliminando un test innecesario, usé arquitectura de test por componente

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { App } from './App';
-
-test('renders realizar prueba button', () => {
-  render(<App />);
-  const buttonElement = screen.getByText(/Realizar Prueba/i);
-  expect(buttonElement).toBeInTheDocument();
-});
